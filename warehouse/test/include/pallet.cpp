1f40afda553faeb8d3ecc6904b76a598b34c4011
MadmouseKed <75257164+MadmouseKed@users.noreply.github.com>
2023-05-30 14:55:01 +0200

    Pallet cpp hpp
    Created Pallet class

--- warehouse/test/include/pallet.cpp
+++ warehouse/test/include/pallet.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
 #include "pallet.hpp"
+Pallet::Pallet(std::string itemName, int itemCapacity, int itemCount){
+    this->itemName = itemName;
+    this->itemCapacity = itemCapacity;
+    this->itemCount = itemCount;
+}
+Pallet::Pallet(){
+    itemName = "Nothing";
+    itemCapacity = 0;
+    itemCount = 0;
+}
+std::string Pallet::getItemName(){
+    return itemName;
+}
+int Pallet::getItemCount(){
+    return itemCount;
+}
+int Pallet::getRemainingSpace(){
+    return itemCapacity - itemCount;
+}
+bool Pallet::reallocateEmptyPallet(std::string itemName, int itemCapacity, int itemCount = 0){
+    this->itemName = itemName;
+    this->itemCapacity = itemCapacity;
+    this->itemCount = itemCount;
+    return true;
+}
+bool Pallet::takeOne(){
+    if(itemCount > 0){
+        itemCount -= 1;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+bool Pallet::putOne(){
+    if(itemCount<itemCapacity){
+        itemCount += 1;
+        return true;
+    }
+    else{
+        return false;
+    }
+}
--- warehouse/test/include/pallet.hpp
+++ warehouse/test/include/pallet.hpp
@@ -5,16 +5,39 @@ class Pallet
         std::string itemName; //only mutable if empty
         int itemCapacity; //only mutable if empty
     public:
-        /// @brief Creates a Pallet object using itemName, itemCapcity and itemCount
-        /// @param itemName 
-        /// @param itemCapacity 
-        /// @param itemCount 
+        /// @brief Creates a Pallet object using itemName, itemCapcity and itemCount, each Pallet can only have 1 kind of item.
+        /// @param itemName the name of the item in the pallet.
+        /// @param itemCapacity how many of the item the pallet could theoreticly hold.
+        /// @param itemCount how many of the item the pallet currently holds.
         Pallet(std::string itemName, int itemCapacity, int itemCount);
+
+        /// @brief Creates an empty pallet, holds placeholder item named "Nothing", with a capacity of 0 and a current item count of 0.
         Pallet();
+
+        /// @brief gets the item name associated with the Pallet
+        /// @return string itemName
         std::string getItemName();
+
+        /// @brief gets the current item count of the pallet
+        /// @return integer itemCount
         int getItemCount();
+
+        /// @brief tells how much empty space remains on the pallet
+        /// @return integer empty space left
         int getRemainingSpace();
-        bool reallocateemptyPallet(std::string itemName, int itemCapacity);
+
+        /// @brief Repurposes a pallet by changing what item it holds and how much of that item.
+        /// @param itemName new item's name
+        /// @param itemCapacity new item's capacity for the pallet
+        /// @param itemCount Optional: new item's amount on the pallet, if not specified defaults to 0
+        /// @return true if function call was succesful
+        bool reallocateEmptyPallet(std::string itemName, int itemCapacity, int itemCount = 0);
+
+        /// @brief attempts to increase itemcount of item by 1, if succesful return True, else return False
+        /// @return successfulness of item count increase
         bool takeOne();
+
+        /// @brief attempts to decrease itemcount of item by 1, if succesful return True, else return False
+        /// @return succesfulness of item count decrease
         bool putOne();
 };
