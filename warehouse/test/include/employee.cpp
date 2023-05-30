ad9d0510fef1bacd65342213d558ba0e61946d67
MadmouseKed <75257164+MadmouseKed@users.noreply.github.com>
2023-05-30 15:11:54 +0200

    Employee class
    created Employee class

--- warehouse/test/include/employee.cpp
+++ warehouse/test/include/employee.cpp
@@ -1,1 +1,23 @@
 #include "employee.hpp"
+#include<string>
+
+Employee::Employee(std::string name, bool forkLiftCertificate = false){
+    this->name = name;
+    this->forkLiftCertificate = forkLiftCertificate;
+    busy = false;
+}
+std::string Employee::getname(){
+    return name;
+}
+bool Employee::getBusy(){
+    return busy;
+}
+void Employee::setBusy(bool busy){
+    this->busy = busy;
+}
+bool Employee::getForkLiftCertificate(){
+    return forkLiftCertificate;
+}
+void Employee::setForkLiftCertificate(bool forkLiftCertificate){
+    this->forkLiftCertificate = forkLiftCertificate;
+}
--- warehouse/test/include/employee.hpp
+++ warehouse/test/include/employee.hpp
@@ -3,12 +3,30 @@ class Employee
     private:
         std::string name;
         bool busy;
-        bool forkliftCertificate;
+        bool forkLiftCertificate;
     public:
-        Employee(std::string name, bool forkliftCertificate);
+        /// @brief creates the Employee object, fills in busy as false.
+        /// @param name name of the employee
+        /// @param forkLiftCertificate Optional: true if the employee has a forklift certificate, false if not. If not provided we assume that they do not have a certificate.
+        Employee(std::string name, bool forkLiftCertificate = false);
+
+        /// @brief gets the name of employee
+        /// @return name
         std::string getname();
+
+        /// @brief gets whether employee is currently bussy or not
+        /// @return true if bussy, false if not
         bool getBusy();
+
+        /// @brief updates employees busy status to provided busy
+        /// @param busy true if bussy, false if not
         void setBusy(bool busy);
+
+        /// @brief gets whether employee has a forklift certificate or not
+        /// @return true if has certificate, false if not
         bool getForkLiftCertificate();
+
+        /// @brief updates employee forklift certificate status to provided forkliftcertificate
+        /// @param forkLiftCertificate true if has certificate, false if not
         void setForkLiftCertificate(bool forkLiftCertificate);
 };
